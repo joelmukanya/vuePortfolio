1258e9d0e6bfffad77fb4c20e40e428790077600
joelmukanya <joel@lifechoices.co.za>
2022-07-17 11:49:57 +0200

    Update on the design

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,13 +1,13 @@
 <template>
-    <div id="footer" class="container-fluid d-flex flex-wrap bg-gradient">
-        <div class="row">
+    <div id="footer" class="container-fluid bg-gradient">
+        <div class="row d-flex flex-wrap">
             <aside class="col-md-4">
                 <h4 class="display-4">Copyright</h4>
                 <p class="lead">Joel's Portofio &copy; 
                     <span class="currYear"></span>
                 </p>
             </aside>
-            <aside class="col-md-5 float-end">
+            <aside class="col-md-5">
                 <h4 class="display-4">Navigation</h4>
                 <ul class="navbar-nav"> 
                     <li class="nav-item">
