8a09ef5f11361e74b00f2a59a538f719f009a081
joelmukanya <joel@lifechoices.co.za>
2022-07-17 21:41:11 +0200

    Form

--- src/views/Contact/Contact.vue
+++ src/views/Contact/Contact.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
     <h2 class="display-2">Contact</h2>
+    <form>
+      <div class="row">
+        <input class="form-control" type="text" placeholder="Name"/>
+      </div>
+      <div class="row">
+        <input class="form-control" type="text" placeholder="Surname"/>
+      </div>
+    </form>
   </div>
 </template>
 
