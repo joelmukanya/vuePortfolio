4b1e6451d59dd1be460e419e44fc37c6576500e5
joelmukanya <joel@lifechoices.co.za>
2022-07-18 12:01:22 +0200

    Updates

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,7 @@
 <template>
     <nav id="header" :class="{headerBgColor: applyStyle}" class="navbar navbar-expand-lg bg-dark navbar-dark container-fluid position-sticky top-0">
         <div class="container">
+            
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon "></span>
             </button>
--- src/views/About/AboutView.vue
+++ src/views/About/AboutView.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <h2 class="display-2">About</h2>
     <LandingPage/>
+    
   </div>
 </template>
 
