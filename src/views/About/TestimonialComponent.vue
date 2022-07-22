9c3161a5b1d9b41fa2140c724559c91d187e1992
joelmukanya <joel@lifechoices.co.za>
2022-07-22 22:18:24 +0200

    Busy

--- src/views/About/TestimonialComponent.vue
+++ src/views/About/TestimonialComponent.vue
@@ -1,16 +1,14 @@
 <template>
   <section id="testimonial" class="container">
     <h2 class="display-2 text-center">Testimonials</h2>
-    <div class="row d-flex flex-wrap justify-content-evenly bg-black my-3">
-        <div id="card-slider" class="col align-self-center" v-for="(testimonial, index) in testimonials" :key="index">
-            <div class="card my-2" style="height: 30rem;">
-                <div class="d-block">
-                    <img :src="testimonial.profile" class="card-img-top testimonial-img" :alt="testimonial.fullname">
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title bg-dark text-white">{{testimonial.fullname}}</h5>
-                    <p class="card-text lead">{{testimonial.description}}</p>
-                </div>
+    <div class="row d-block d-md-flex flex-wrap justify-content-around bg-black my-3">
+        <div class="card col-md" v-for="(testimonial, index) in testimonials" :key="index">
+            <div class="d-block">
+                <img :src="testimonial.profile" class="card-img-top testimonial-img" :alt="testimonial.fullname">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title bg-dark text-white">{{testimonial.fullname}}</h5>
+                <p class="card-text lead">{{testimonial.description}}</p>
             </div>
         </div>
     </div>
